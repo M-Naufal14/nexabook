9962f4646bb9283df8e9ee0febdb1efd076c67b7
M-Naufal14 <muhammadnaufak204@gmail.com>
2026-04-29 23:44:02 +0700

    bismillah

---
+++ android/app/src/main/kotlin/com/example/nexabook/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nexabook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.blue,
+      body: SizedBox(), 
+    );
+  }
+}
